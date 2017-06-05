a774b2e8ab3170be04cd22236bebdb3e040e78e1
天久秀樹 <h_ameku@funteam.co.jp>
2017-06-05 14:00:00 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,2 +1,13 @@
+var sortNumber = funciton (number) {
+  number.sort(funciton (a,b) {
+    if (a == b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  });
+}
+
 var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
+
